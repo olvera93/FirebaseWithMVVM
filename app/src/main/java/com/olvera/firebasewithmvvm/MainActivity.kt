5351619065c527cf30b36b24004fdcd670622e67
olvera93 <olvera9302@outlook.com>
2022-08-25 13:39:00 -0500

    [ADD] Firebase firestore services and create a new collection users

--- app/src/main/java/com/olvera/firebasewithmvvm/MainActivity.kt
+++ app/src/main/java/com/olvera/firebasewithmvvm/MainActivity.kt
@@ -2,10 +2,27 @@ package com.olvera.firebasewithmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Gonzalo"
+        user["last"] = "Olvera"
+        user["born"] = 1999
+
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG", "DocumentSnaphot added wit ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e -> Log.w("TAG", "Error adding document: $e") }
+
+
     }
 }
