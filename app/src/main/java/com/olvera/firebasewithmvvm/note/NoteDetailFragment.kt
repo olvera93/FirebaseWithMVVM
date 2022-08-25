6529b89541367db4a3bffa9426aef5047a44ca8f
olvera93 <olvera9302@outlook.com>
2022-08-25 14:24:19 -0500

    [ADD] Navigation component

---
+++ app/src/main/java/com/olvera/firebasewithmvvm/note/NoteDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.olvera.firebasewithmvvm.note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.olvera.firebasewithmvvm.R
+import com.olvera.firebasewithmvvm.databinding.FragmentNoteDetailBinding
+
+class NoteDetailFragment : Fragment() {
+
+    val TAG: String = "NoteDetailFragment"
+    lateinit var binding: FragmentNoteDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNoteDetailBinding.inflate(layoutInflater)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
